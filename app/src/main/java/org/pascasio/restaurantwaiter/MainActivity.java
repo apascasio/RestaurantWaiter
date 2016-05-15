3a59af09c7544d235cb79f94150235d551ff4a77
Andres Pascasio <andrespascasio@gmail.com>
2016-05-14 19:43:03 -0600

    first commit

---
+++ app/src/main/java/org/pascasio/restaurantwaiter/MainActivity.java
@@ -0,0 +1,13 @@
+package org.pascasio.restaurantwaiter;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
